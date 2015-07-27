98a299b31e947a6a9c594353b9bcb5a55e0a14ac
Munsterberg <jetrussell93@gmail.com>
2015-07-27 04:07:17 -0400

    Funcional test changes

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in broswer.title
+  def setUp(self):
+    self.browser = webdriver.Firefox()
+    self.browser.implicitly_wait(3)
+
+  def tearDown(self):
+    self.browser.quit()
+
+  def test_can_start_a_list_and_retrieve_it_later(self):
+    # user opens browser and navigates to URL
+    self.browser.get('http://localhost:8000')
+
+    # user notices the page title
+    self.assertIn('To-Do', self.broswer.title)
+    self.fail('Finish the test!')
+
+if __name__ == '__main__':
+  unittest.main(warnings='ignore')
