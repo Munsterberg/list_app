34fc5a9cdaa71e501b58b195a14f9540480f44cd
Munsterberg <jetrussell93@gmail.com>
2015-07-27 11:23:46 -0400

    Test addition

--- lists/tests.py
+++ lists/tests.py
@@ -20,3 +20,12 @@ class HomePageTest(TestCase):
 
     self.assertEqual(response.content.decode(), expected_html)
 
+  def test_home_page_can_save_a_post_request(self):
+    request = HttpRequest()
+    request.method = 'POST'
+    request.post['item_text'] = 'A new list item'
+
+    response = home_page(request)
+
+    self.assertIn('A new list item', response.content.decode())
+
